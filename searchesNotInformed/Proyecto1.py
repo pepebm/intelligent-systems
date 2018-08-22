86ad99f72d0f63d0a1a1c322e60c830a58c166f0
José Manuel <jose@thegurucompany.com>
2018-08-22 11:18:04 -0500

    Helper functions

--- searchesNotInformed/Proyecto1.py
+++ searchesNotInformed/Proyecto1.py
@@ -1,16 +1,31 @@
-from Queue import Queue
+from queue import Queue
 
 def busquedaNoInformada(edoInicial, edoFinal, algoritmo):
     result = ''
     if algoritmo == 0:
-        result = bfsImplementation(edoInicial, edoFinal)
+        bfsImplementation(edoInicial, edoFinal)
     else:
-        result = dfsImplementation(edoInicial, edoFinal)
+        dfsImplementation(edoInicial, edoFinal)
     return result
 
-def bfsImplementation(start, end):
-    queue = Queue()
+def bfsImplementation(current, end):
+    q = Queue()
+    zeroPosition = _findZero(current)
+    #while current != end:
 
-def dfsImplementation(start, end):
+def dfsImplementation(current, end):
     stack = []
 
+
+def _findZero(arr):
+    for i in range(len(arr)):
+        try:
+            idx = arr[i].index(0)
+            return [i, idx]
+        except ValueError:
+            pass
+
+def _swap(startX, startY, finishX, finishY, arr):
+    temp = arr[startX][startY]
+    arr[startX][startY] = arr[finishX][finishY]
+    arr[finishX][finishY] = temp
