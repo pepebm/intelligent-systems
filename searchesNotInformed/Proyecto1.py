3a64e4e0ee6af7e6c8399611f53b3967de4f2f61
José Manuel <jose@thegurucompany.com>
2018-08-26 23:49:20 -0500

    bfs not working (bugs)

--- searchesNotInformed/Proyecto1.py
+++ searchesNotInformed/Proyecto1.py
@@ -1,31 +1,120 @@
 from queue import Queue
 
+# Made by: Jose Manuel Beauregard Mendez - A01021716
+
+
+# main function
 def busquedaNoInformada(edoInicial, edoFinal, algoritmo):
     result = ''
     if algoritmo == 0:
-        bfsImplementation(edoInicial, edoFinal)
+        result = bfsImplementation(edoInicial, edoFinal)
     else:
         dfsImplementation(edoInicial, edoFinal)
     return result
 
+# Breadth-first search approach with a queue
 def bfsImplementation(current, end):
-    q = Queue()
+    initial = current
     zeroPosition = _findZero(current)
-    #while current != end:
+    firstSteps = _checkMove(zeroPosition, [])
+    code = 0
+    while not firstSteps.empty():
+        visited = []
+        moves = Queue()
+        moves.put(firstSteps.get())
+        result = ''
+        while not moves.empty():
+            nextMove = moves.get()
+            print(nextMove)
+            current = _swap(zeroPosition, nextMove, current)
+            visited.append(nextMove)
+            result += ('- ' + nextMove[2] + ' ->')
+            if current == end:
+                code = 1
+                break
+            zeroPosition = _findZero(current)
+            moves = _mergeQueues(moves, _checkMove(zeroPosition, visited))
+        if code == 1: break
+    if code != 1: result = 'No solution found'
+    return result
 
+# Depth-first search approach with a stack (Python list)
 def dfsImplementation(current, end):
     stack = []
 
-
+# Helper function that will return X & Y position of Zero
 def _findZero(arr):
     for i in range(len(arr)):
         try:
             idx = arr[i].index(0)
             return [i, idx]
+        # Expecting failure if index is not found
         except ValueError:
             pass
 
-def _swap(startX, startY, finishX, finishY, arr):
+# Helper function to swap elements in the 2d list 
+def _swap(start, finish, arr):
+    startX, startY = start[0], start[1]
+    finishX, finishY = finish[0], finish[1]
     temp = arr[startX][startY]
     arr[startX][startY] = arr[finishX][finishY]
     arr[finishX][finishY] = temp
+    return arr
+
+# Helper funcition that will check possible moves given the zero position
+def _checkMove(zeroPosition, visited, algorithm = 0):
+    if algorithm == 0:
+        x = zeroPosition[0]
+        y = zeroPosition[1]
+        idx = (x * 3) + y
+        nextMoves = Queue()
+        # left
+        print(idx)
+        if idx % 3 > 0:
+            if [x - 1, y] not in visited:
+                nextMoves.put([x - 1, y, 'L'])
+        # down
+        if idx < 6:
+            if [x, y + 1] not in visited:
+                nextMoves.put([x, y + 1, 'D'])
+        # right
+        if idx % 3 < 2:
+            if [x + 1, y] not in visited:
+                nextMoves.put([x + 1, y, 'R'])
+        # up
+        if idx > 2:
+            if [x, y - 1] not in visited:
+                nextMoves.put([x, y - 1, 'U'])
+    else:
+        nextMoves = _checkMoveList(zeroPosition, visited)
+    return nextMoves
+
+
+def _checkMoveList(zeroPosition, visited):
+    x = zeroPosition[0]
+    y = zeroPosition[1]
+    idx = (x * 3) + y
+    nextMoves = []
+    # left
+    if idx % 3 > 0:
+        if [x - 1, y] not in visited:
+            nextMoves.append([x - 1, y, 'L'])
+    # down
+    if idx < 6:
+        if [x, y + 1] not in visited:
+            nextMoves.append([x, y + 1, 'D'])
+    # right
+    if idx % 3 < 2:
+        if [x + 1, y] not in visited:
+            nextMoves.append([x + 1, y, 'R'])
+    # up
+    if idx > 2:
+        if [x, y - 1] not in visited:
+            nextMoves.append([x, y - 1, 'U'])
+    return nextMoves
+
+# Helper function that will put b's values in a Queue
+def _mergeQueues(a, b):
+    while not b.empty():
+        a.put(b.get())
+    return a
--- searchesNotInformed/test.py
+++ searchesNotInformed/test.py
@@ -1,5 +1,5 @@
 from Proyecto1 import busquedaNoInformada
 edoInicial = [[0, 1, 2], [4, 5, 3], [7, 8, 6]]
 edoFinal = [[1, 2, 3], [4, 5, 6], [7, 8, 0]]
-algoritmo = 0   # donde X puede ser 0 para BFS o 1 para DFS
+algoritmo = 0   # puede ser 0 para BFS o 1 para DFS
 print(busquedaNoInformada(edoInicial, edoFinal, algoritmo))
