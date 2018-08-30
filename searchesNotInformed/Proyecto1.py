ac28b723dfeb01389b98edc6e0659a4d4e906366
José Manuel <jose@thegurucompany.com>
2018-08-30 18:26:58 -0500

    bfs & dfs working

--- searchesNotInformed/Proyecto1.py
+++ searchesNotInformed/Proyecto1.py
@@ -1,26 +1,36 @@
-from queue import Queue
-import pry
-
 # 8 puzzle solver, implementation with BFS and DFS
-# Made by: Jose Manuel Beauregard Mendez - 
+# Made by: Jose Manuel Beauregard Mendez - A01021716
 
 # Node class that will represent a movement with it's parent step
 class Node:
-    route = ''
-    zero = []
-
-    def __init__(self, parent, zeroPosition, direction, move='X'):
+    def __init__(self, parent, zeroPosition, direction):
         self.parent = parent
-        self.move = move
         self.direction = direction
         self.zero = zeroPosition
+        self.route = ''
     
+    # Function that will make the move based on the direction of the Node
     def makeMovement(self):
-        pry()
-        if self.move != 'X':
-            self.current = _swap(self.zero, [self.move[0], self.move[1]], self.parent.current)
-            if self.direction != '':
-                self.route += self.parent.route + ('-' + self.direction + '-> ')
+        self.current = _swap(self.zero, self.calculateMove(), self.parent.current)
+        # Validates non valid moves
+        if (self.direction != '') or (self.current != self.parent.current):
+            self.route += self.parent.route + ('-' + self.direction + '-> ')
+    # Function that will return [x,y] position of the new move
+    def calculateMove(self):
+        zero = self.zero
+        # Validates non valid moves
+        if self.direction == '':
+            newPos = zero
+        else:
+            if self.direction == 'R':
+                newPos = [zero[0], zero[1] + 1]
+            elif self.direction == 'D':
+                newPos = [zero[0] + 1, zero[1]]
+            elif self.direction == 'L':
+                newPos = [zero[0], zero[1] - 1]
+            elif self.direction == 'U':
+                newPos = [zero[0] - 1, zero[1]]
+        return newPos
 
 # main function
 def busquedaNoInformada(edoInicial, edoFinal, algoritmo):
@@ -31,18 +41,17 @@ def busquedaNoInformada(edoInicial, edoFinal, algoritmo):
         if algoritmo == 0:
             result = bfsImplementation(edoInicial, edoFinal)
         else:
-            dfsImplementation(edoInicial, edoFinal)
+            result = dfsImplementation(edoInicial, edoFinal)
     return result
 
 # Breadth-first search approach with a queue
 def bfsImplementation(initial, end):
     # Initializing
     sol = 0
-    cost = 0
     result = ''
     zero = _findZero(initial)
     # Add initial state manually
-    nodes = [Node(initial, zero, '', zero)]
+    nodes = [Node(initial, zero, '')]
     nodes[0].current = initial
     nodes[0].route = ''
     nodes[0].parent = nodes[0]
@@ -53,60 +62,62 @@ def bfsImplementation(initial, end):
     while len(nodes) != 0:
         # FIFO
         node = nodes.pop(0)
+        node.makeMovement()
+        if not _checkVisited(visited, node):
+            # Add new node position
+            visited.append(node.current)
+        else:
+            # If node has already been visited skip this iteration
+            continue
         # Check if we have the solution
-        if node.parent.current == end:
+        if node.current == end:
             sol = 1
             result = node.route
             break
         # Find the possible moves and make a Node for each one
         for m in _checkMove(node.zero, visited):
             # Build new branches of the tree
-            nodes.append(Node(node, _findZero(node.parent.current), m.pop(), m))
-        # Alter current node board before checking its new children node's
-        node.makeMovement()
-        print(node.route)
-        # Add current node position, unless it's the first iteration
-        visited.append([node.move[0], node.move[1]])
-        # Add step
-        cost += 1
-    if sol != 1: result = 'No solution found'
+            nodes.append(Node(node, _findZero(node.current), m))
+    if sol != 1: 
+        result = 'No solution found'
     return result
 
 # Depth-first search approach with a stack (Python list)
-def dfsImplementation(current, end):
-    initial = current
-    zeroPosition = _findZero(current)
-    firstSteps = _checkMove(zeroPosition, [zeroPosition], 1)
+def dfsImplementation(initial, end):
+    # Initializing
     sol = 0
-    while not firstSteps.empty():
-        # This array will have the visited positions
-        visited = [zeroPosition]
-        # Structure were we'll save the movements
-        moves = []
-        # Create all the possible ways to start
-        moves.append(firstSteps.pop())
-        # String were we'll save the final result, if any
-        result = ''
-        while not moves.empty():
-            nextMove = moves.pop()
-            _swap(zeroPosition, nextMove, current)
-            visited.append([nextMove[0], nextMove[1]])
-            result += ('-' + nextMove[2] + '-> ')
-            print(result)
-            print(current)
-            print(moves.queue)
-            # Check if we have the solution
-            if current == end:
-                sol = 1
-                break
-            # Get new position
-            zeroPosition = _findZero(current)
-            print(zeroPosition)
-            # Find new movements, if none found and moves is empty, the while will end
-            moves = moves + _checkMove(zeroPosition, visited)
-        # Stop finding paths if solution is found
-        if sol == 1: break
-    if sol != 1: result = 'No solution found'
+    result = ''
+    zero = _findZero(initial)
+    # Add initial state manually
+    nodes = [Node(initial, zero, '')]
+    nodes[0].current = initial
+    nodes[0].route = ''
+    nodes[0].parent = nodes[0]
+    # Add current node position as visited
+    visited = []
+    # Loop until every possible solution is calculated,
+    # if a solution is found, the cycle breaks
+    while len(nodes) != 0:
+        # LIFO
+        node = nodes.pop()
+        node.makeMovement()
+        if not _checkVisited(visited, node):
+            # Add new node position
+            visited.append(node.current)
+        else:
+            # If node has already been visited skip this iteration
+            continue
+        # Check if we have the solution
+        if node.current == end:
+            sol = 1
+            result = node.route
+            break
+        # Find the possible moves and make a Node for each one
+        for m in _checkMove(node.zero, visited):
+            # Build new branches of the tree
+            nodes.append(Node(node, _findZero(node.current), m))
+    if sol != 1:
+        result = 'No solution found'
     return result
 
 # Helper function that will return X & Y position of Zero
@@ -120,54 +131,55 @@ def _findZero(arr):
 
 # Helper funcition that will check possible moves given the zero position
 def _checkMove(zeroPosition, visited):
-    print('v')
-    print(visited)
     x = zeroPosition[0]
     y = zeroPosition[1]
     # This formula will create a sequential id for each position given their coordinates
     idx = (x * 3) + y
-    #print('zero id: '+ str(idx))
     nextMoves = []
-    # right
-    if idx % 3 < 2:
-        if not _checkVisited(visited, [x, y + 1]):
-            nextMoves.append([x, y + 1, 'R'])
     # down
     if idx < 6:
-        if not _checkVisited(visited, [x + 1, y]):
-            nextMoves.append([x + 1, y, 'D'])
-    # left
-    if idx % 3 > 0:
-        if not _checkVisited(visited, [x, y - 1]):
-            nextMoves.append([x, y - 1, 'L'])
+        nextMoves.append('D')
     # up
     if idx > 2:
-        if not _checkVisited(visited, [x - 1, y]):
-            nextMoves.append([x - 1, y, 'U'])
+        nextMoves.append('U')
+    # left
+    if idx % 3 > 0:
+        nextMoves.append('L')
+    # right
+    if idx % 3 < 2:
+        nextMoves.append('R')
     return nextMoves
-
 
 # Helper function that will check if the move has already been done
 # return True if found
-def _checkVisited(visited, move):
+def _checkVisited(visited, node):
     result = False
-    for pos in visited:
-        if move == pos: 
+    for idx, item in enumerate(visited):
+        if item == node.current:
             result = True
     return result
 
 # Helper function to swap elements in the 2d list
 def _swap(start, finish, arr):
-    print('Move ' + str(start[0]) + str(start[1]) +
-         ' to ' + str(finish[0]) + str(finish[1]))
-    print('s')
-    print(arr)
-    newBoard = arr
+    if (finish[0] < 0 or finish[0] > 2) or (finish[1] < 0 or finish[1] > 2):
+        return arr
+    newBoard = _deepCopy(arr)
     startX, startY = start[0], start[1]
     finishX, finishY = finish[0], finish[1]
     temp = newBoard[startX][startY]
     newBoard[startX][startY] = newBoard[finishX][finishY]
     newBoard[finishX][finishY] = temp
-    print('f')
-    print(newBoard)
     return newBoard
+
+# Helper function that will correctly copy the 8 puzzle board
+def _deepCopy(arr):
+    result = [[], [], []]
+    for idx, item in enumerate(arr):
+        result[idx] = item.copy()
+    return result
+
+# Helper function that will print the 2d formatted in a 8 puzzle board
+def _printMatrix(arr):
+    for i in arr:
+        print(i)
+    print('')
--- searchesNotInformed/test.py
+++ searchesNotInformed/test.py
@@ -1,5 +1,15 @@
 from Proyecto1 import busquedaNoInformada
+
+# edoInicial = [[1, 2, 3], [8, 0, 4], [7, 6, 5]]
+# edoFinal = [[1, 3, 4], [8, 0, 5], [7, 2, 6]]
+
+# edoInicial = [[1, 3, 4], [8, 6, 2], [0, 7, 5]]
+# edoFinal = [[1, 2, 3], [8, 0, 4], [7, 6, 5]]
+
 edoInicial = [[0, 1, 2], [4, 5, 3], [7, 8, 6]]
 edoFinal = [[1, 2, 3], [4, 5, 6], [7, 8, 0]]
-algoritmo = 0   # puede ser 0 para BFS o 1 para DFS
+
+# edoInicial = [[0, 1, 2], [4, 5, 3], [7, 8, 6]]
+# edoFinal = [[1, 2, 0], [4, 5, 3], [7, 8, 6]]
+algoritmo = 1   # puede ser 0 para BFS o 1 para DFS
 print(busquedaNoInformada(edoInicial, edoFinal, algoritmo))
