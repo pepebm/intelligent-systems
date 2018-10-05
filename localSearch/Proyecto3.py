371a5ec2ad6de04b5eb58bc686f83bd41346db0e
José Manuel <jose@thegurucompany.com>
2018-10-04 21:09:12 -0500

    Fixed bugs, sometimes its right... Others no too much

--- localSearch/Proyecto3.py
+++ localSearch/Proyecto3.py
@@ -1,11 +1,20 @@
+"""
+    Made by: José Manuel Beauregard Méndez - A0101716
+    ---- N - Queen problem with Hill Climbing ----
+        · Heuristic: number of queens in attack position
+"""
+
 from random import randint
 from copy import deepcopy
 
+# TODO DELETE THIS IMPORT
+import pry
+
 def busquedaHC(N, lateral, M):
     if lateral:
-        print(sideMoveEnabled(N, M))
+        sideMoveEnabled(N, M)
     else:
-        print(sideMoveDisabled(N, M))
+        sideMoveDisabled(N, M)
 
 # This function will count the number of queens that are in
 # attack position, that number will be the heuristic value
@@ -19,11 +28,11 @@ def heuristic(b):
             # Check row
             if(qPos[j][0] == cQueen[0]):
                 h += 1
-                continue
             # Check column
             if(qPos[j][1] == cQueen[1]):
                 h += 1
-                continue
+    print("h: ", h)
+    printBoard(b)
     return h
 
 # This function will count the number of queens that are in
@@ -38,94 +47,95 @@ def heuristic_Diagonal(b):
             # Check row
             if(qPos[j][0] == cQueen[0]):
                 h += 1
-                continue
             # Check column
             if(qPos[j][1] == cQueen[1]):
                 h += 1
-                continue
             # Check diagonally
             if(abs(cQueen[0] - qPos[j][0]) == abs(cQueen[1] - qPos[j][1])):
                 h += 1
-                continue
+    print("h: ", h)
+    printBoard(b)
     return h
 
 # Hill climbing algorithm without diagonal move's
 def sideMoveDisabled(N, M):
-    board = createBoard(N)
-    visited = []
-    val = heuristic(board)
-    qPos = findQueens(board)
-    bestVal = board
-    while(val != 0):
-        print('*'*15,' Round - ', M, ' ', '*'*15)
-        M -= 1
-        if not M > -1:
-            bestVal = 'No solution found'
-            break
-        for i in range(len(qPos)):
-            neighbor = qPos.pop(0)
-            moves = searchMoves(N, neighbor)
-            for j in range(len(moves)):
-                tempBoard = generateMove(neighbor, moves[j], board)
-                if(checkVisited(visited, tempBoard)): continue
-                neighborVal = heuristic(board)
-                if(neighborVal < val):
-                    bestVal = tempBoard
-                    val = neighborVal
-            visited.append(tempBoard)
-        board = bestVal
+    for reps in range(M):
+        print('*'*15,' Round - ', reps, ' ', '*'*15)
+        board = createBoard(N)
+        visited = []
+        val = heuristic(board)
         qPos = findQueens(board)
-    return bestVal
+        bestVal = board
+        while(val != 0):
+            for i in range(len(qPos)):
+                neighbor = qPos.pop(0)
+                moves = searchMoves(N, neighbor)
+                for j in range(len(moves)):
+                    tempBoard = generateMove(neighbor, moves[j], board)
+                    if checkVisited(visited, tempBoard): continue
+                    neighborVal = heuristic(tempBoard)
+                    if neighborVal == 0:
+                        bestVal = tempBoard
+                        printBoard(tempBoard)
+                        return
+                    if neighborVal < val:
+                        bestVal = tempBoard
+                        val = neighborVal
+                visited.append(tempBoard)
+            board = bestVal
+            qPos = findQueens(board)
+    print('No solution found')
 
 # Hill climbing algorithm with diagonal move's
 def sideMoveEnabled(N, M):
-    board = createBoard(N)
-    visited = []
-    val = heuristic_Diagonal(board)
-    qPos = findQueens(board)
-    bestVal = board
-    while(val != 0):
-        print('*'*15,' Round - ', M, ' ', '*'*15)
-        M -= 1
-        if not M > -1:
-            bestVal = 'No solution found'
-            break
-        for i in range(len(qPos)):
-            neighbor = qPos.pop(0)
-            moves = searchMoves(N, neighbor)
-            for j in range(len(moves)):
-                tempBoard = generateMove(neighbor, moves[j], board)
-                if(checkVisited(visited, tempBoard)): continue
-                neighborVal = heuristic_Diagonal(board)
-                if(neighborVal < val):
-                    bestVal = tempBoard
-                    val = neighborVal
-            visited.append(tempBoard)
-        board = bestVal
+    for reps in range(M):
+        print('*'*15,' Round - ', reps, ' ', '*'*15)
+        board = createBoard(N)
+        visited = []
+        val = heuristic_Diagonal(board)
         qPos = findQueens(board)
-    return bestVal
+        bestVal = board
+        while(val != 0):
+            for i in range(len(qPos)):
+                neighbor = qPos.pop(0)
+                moves = searchMoves(N, neighbor)
+                for j in range(len(moves)):
+                    tempBoard = generateMove(neighbor, moves[j], board)
+                    if checkVisited(visited, tempBoard): continue
+                    neighborVal = heuristic_Diagonal(tempBoard)
+                    if neighborVal == 0:
+                        bestVal = tempBoard
+                        printBoard(tempBoard)
+                        return
+                    if neighborVal < val:
+                        bestVal = tempBoard
+                        val = neighborVal
+                visited.append(tempBoard)
+            board = bestVal
+            qPos = findQueens(board)
+    print('No solution found')
 
 # Helper function that will make a deepCopy of the current board &
 # create a new one with the move passed
 def generateMove(start, finish, b):
-    print("old board")
-    printBoard(b)
+    # print("old board")
+    # printBoard(b)
     new_b = deepcopy(b)
     new_b[start[0]][start[1]], new_b[finish[0]][finish[1]] = 0, 1
-    print("new board")
-    printBoard(new_b)
+    # print("new board")
+    # printBoard(new_b)
     return new_b
 
 # Helper function that will check if current board has been visited previously
 def checkVisited(visited, b):
     result = False
-    for i in range(len(visited)): 
-        if(visited[i] == b): result = True
+    for i, item in enumerate(visited): 
+        if(item == b): 
+            result = True
     return result
 
 # Helper function that will return the possible moves of a given Queen
 def searchMoves(N, pos):
-    print(pos)
     moves = []
     # UP
     if (pos[0] - 1 >= 0): moves.append([pos[0]-1, pos[1]])
@@ -141,6 +151,7 @@ def findQueens(b):
         for j in range(len(b)):
             if b[i][j] == 1: 
                 pos.append([i, j])
+    #pry()
     return pos
 
 # Helper function that will return a Board with randomly calculated Queens
--- localSearch/main.py
+++ localSearch/main.py
@@ -2,5 +2,5 @@ from Proyecto3 import busquedaHC
 
 N = 8     # o el número de reinas que tenga el problema
 lateral = True # o false
-M = 5     # o el número máximo que se desee reiniciar
+M = 10     # o el número máximo que se desee reiniciar
 busquedaHC(N, lateral, M)
