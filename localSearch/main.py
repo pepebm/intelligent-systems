ca43fc32a8cfbe829fb504db4a0ee07358bcd59b
José Manuel <jose@thegurucompany.com>
2018-10-05 17:08:53 -0500

    Doc

--- localSearch/main.py
+++ localSearch/main.py
@@ -1,6 +1,6 @@
 from Proyecto3 import busquedaHC
 
 N = 8     # o el número de reinas que tenga el problema
-lateral = True # o false
+lateral = False # o false
 M = 38000     # o el número máximo que se desee reiniciar
 busquedaHC(N, lateral, M)
