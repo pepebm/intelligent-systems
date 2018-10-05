9821e202c9bd08130daf9ac4ad30e6e877fcbaed
José Manuel <jose@thegurucompany.com>
2018-10-04 19:27:07 -0500

    Bugs

--- localSearch/Proyecto3.py
+++ localSearch/Proyecto3.py
@@ -1,84 +1,153 @@
+from random import randint, seed
+from copy import deepcopy
+
 def busquedaHC(N, lateral, M):
+    seed(1)
     if lateral:
-        sideMoveEnabled(N, M)
+        print(sideMoveEnabled(N, M))
     else:
-        sideMoveDisabled(N, M)
+        print(sideMoveDisabled(N, M))
 
+# This function will count the number of queens that are in
+# attack position, that number will be the heuristic value
+# that will determine the next action
+def heuristic(b):
+    h = 0
+    qPos = findQueens(b)
+    for i in range(len(qPos)):
+        cQueen = qPos.pop(0)
+        for j in range(len(qPos)):
+            # Check row
+            if(qPos[j][0] == cQueen[0]):
+                h += 1
+                continue
+            # Check column
+            if(qPos[j][1] == cQueen[1]):
+                h += 1
+                continue
+    return h
+
+# This function will count the number of queens that are in
+# attack position, that number will be the heuristic value
+# that will determine the next action. This func will check diagonally
+def heuristic_Diagonal(b):
+    h = 0
+    qPos = findQueens(b)
+    for i in range(len(qPos)):
+        cQueen = qPos.pop(0)
+        for j in range(len(qPos)):
+            # Check row
+            if(qPos[j][0] == cQueen[0]):
+                h += 1
+                continue
+            # Check column
+            if(qPos[j][1] == cQueen[1]):
+                h += 1
+                continue
+            # Check diagonally
+            if(abs(cQueen[0] - qPos[j][0]) == abs(cQueen[1] - qPos[j][1])):
+                h += 1
+                continue
+    return h
+
+# Hill climbing algorithm without diagonal move's
 def sideMoveDisabled(N, M):
-    #board = createBoard(N)
-    solveNQ(N)
+    board = createBoard(N)
+    visited = []
+    val = heuristic(board)
+    qPos = findQueens(board)
+    bestVal = board
+    while(val != 0):
+        print('*'*15,' Round - ', M, ' ', '*'*15)
+        M -= 1
+        if not M >= 0:
+            bestVal = 'No solution found'
+            break
+        for i in range(len(qPos)):
+            neighbor = qPos.pop(0)
+            moves = searchMoves(N, neighbor)
+            for j in range(len(moves)):
+                tempBoard = generateMove(neighbor, moves[j], board)
+                if(checkVisited(visited, tempBoard)): continue
+                neighborVal = heuristic(board)
+                if(neighborVal < val):
+                    bestVal = tempBoard
+                    val = neighborVal
+            visited.append(tempBoard)
+        board = bestVal
+        qPos = findQueens(board)
+    return bestVal
 
+# Hill climbing algorithm with diagonal move's
 def sideMoveEnabled(N, M):
-    board = createBoard()
+    board = createBoard(N)
+    visited = []
+    val = heuristic_Diagonal(board)
+    qPos = findQueens(board)
+    bestVal = board
+    while(val != 0):
+        print('*'*5,' Round - ', val, '*'*5)
+        for i in range(len(qPos)):
+            neighbor = qPos.pop(0)
+            moves = searchMoves(N, neighbor)
+            for j in range(len(moves)):
+                tempBoard = generateMove(neighbor, moves[j], board)
+                if(checkVisited(visited, tempBoard)): continue
+                neighborVal = heuristic_Diagonal(board)
+                if(neighborVal < val):
+                    bestVal = tempBoard
+                    val = neighborVal
+            visited.append(tempBoard)
+        board = bestVal
+        qPos = findQueens(board)
+    return bestVal
 
-def printSolution(board, N): 
-    for i in range(N):
-        for j in range(N): 
-            print(board[i][j], end=' ')
-        print()
-# A utility function to check if a queen can 
-# be placed on board[row][col]. Note that this 
-# function is called when "col" queens are 
-# already placed in columns from 0 to col -1. 
-# So we need to check only left side for 
-# attacking queens 
-def isSafe(board, row, col, N): 
-    # Check this row on left side 
-    for i in range(col): 
-        if board[row][i] == 1: 
-            return False
-    # Check upper diagonal on left side 
-    for i,j in zip(range(row,-1,-1), range(col,-1,-1)): 
-        if board[i][j] == 1: 
-            return False
-    # Check lower diagonal on left side 
-    for i,j in zip(range(row,N,1), range(col,-1,-1)): 
-        if board[i][j] == 1: 
-            return False
-    return True
+# Helper function that will make a deepCopy of the current board &
+# create a new one with the move passed
+def generateMove(start, finish, b):
+    print("old board")
+    printBoard(b)
+    new_b = deepcopy(b)
+    new_b[start[0]][start[1]], new_b[finish[0]][finish[1]] = 0, 1
+    print("new board")
+    printBoard(new_b)
+    return new_b
 
-def solveNQUtil(board, col, N): 
-    # base case: If all queens are placed 
-    # then return true 
-    if col >= N: 
-        return True
+# Helper function that will check if current board has been visited previously
+def checkVisited(visited, b):
+    result = False
+    for i in range(len(visited)): 
+        if(visited[i] == b): result = True
+    return result
 
-    # Consider this column and try placing 
-    # this queen in all rows one by one 
-    for i in range(N): 
-        if isSafe(board, i, col, N): 
-            # Place this queen in board[i][col] 
-            board[i][col] = 1
-            # recur to place rest of the queens 
-            if solveNQUtil(board, col+1, N) == True: 
-                return True
-            # If placing queen in board[i][col 
-            # doesn't lead to a solution, then 
-            # queen from board[i][col] 
-            board[i][col] = 0
-    # if the queen can not be placed in any row in 
-    # this colum col  then return false 
-    return False
-# This function solves the N Queen problem using 
-# Backtracking. It mainly uses solveNQUtil() to 
-# solve the problem. It returns false if queens 
-# cannot be placed, otherwise return true and 
-# placement of queens in the form of 1s. 
-# note that there may be more than one 
-# solutions, this function prints one  of the 
-# feasible solutions. 
-def solveNQ(N): 
-    board = [ [0, 0, 0, 0, 0, 0, 0, 0],
-              [0, 0, 0, 0, 0, 0, 0, 0], 
-              [0, 0, 0, 0, 0, 0, 0, 0], 
-              [0, 0, 0, 0, 0, 0, 0, 0],
-              [0, 0, 0, 0, 0, 0, 0, 0],
-              [0, 0, 0, 0, 0, 0, 0, 0],
-              [0, 0, 0, 0, 0, 0, 0, 0],
-              [0, 0, 0, 0, 0, 0, 0, 0],
-             ] 
-    if solveNQUtil(board, 0, N) == False: 
-        print("Solution does not exist")
-        return False
-    printSolution(board, N) 
-    return True
+# Helper function that will return the possible moves of a given Queen
+def searchMoves(N, pos):
+    print(pos)
+    moves = []
+    # UP
+    if (pos[0] - 1 >= 0): moves.append([pos[0]-1, pos[1]])
+    # Down
+    elif (pos[0] + 1 <= N - 1): moves.append([pos[0]+1, pos[1]])
+    return moves
+
+# Helper function that will return a list of list's with the different
+# Queens position
+def findQueens(b):
+    pos = []
+    for i in range(len(b)):
+        for j in range(len(b)):
+            if b[i][j] == 1: 
+                pos.append([i, j])
+    return pos
+
+# Helper function that will return a Board with randomly calculated Queens
+def createBoard(N):
+    b = [[0] * N for i in range(N)]
+    for i in range(N): b[randint(0, N)][i] = 1
+    return b
+
+# Helper function that will print the board passed
+def printBoard(b):
+    for i in b: print(i)
+    print(' ')
+
