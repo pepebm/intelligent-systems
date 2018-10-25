b581a8a667aa1e288b449820ac1b2787d2bae87f
José Manuel <jose@thegurucompany.com>
2018-10-25 17:36:52 -0500

    Docs

--- minimax/test.py
+++ minimax/test.py
@@ -1,2 +1,5 @@
 from Proyecto4 import othello
-othello(1,0,0)
+# nivel: 1 o 2
+# fichas: 0 computadora juega blancas, 1 computadora juega negras
+# inicio: 0 inicia la computadora, 1 inicia el jugador
+othello(nivel = 2, fichas = 0, inicio = 0)
